b2e3c44a64045cde9a2a5406cf2b8ef009145158
RiosHectorM <rios.hector.martin@gmail.com>
2023-10-27 05:00:03 -0300

    'images'

---
+++ app/components/BlockImage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const BlockImage = () => {
+  return (
+    <div>
+      <div>telaraña 1</div>
+      <div>telaraña 2</div>
+      
+      <div>telaraña 3</div>
+      <div>telaraña 4</div>
+    </div>
+  );
+}
+
+export default BlockImage
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import MapaHome from './components/MapaHome';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center ">
+      <BlockImage />
       <CardSofi />
       <MapaHome />
     </main>
